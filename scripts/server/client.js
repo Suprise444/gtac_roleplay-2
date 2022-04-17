5aea4293b807263e8f5cbb251d1ee336b998077a
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-04-16 21:46:12 -0500

    Fix animations

--- scripts/server/client.js
+++ scripts/server/client.js
@@ -987,54 +987,22 @@ function sendPlayerEnterPropertyKey(client, key) {
 // ===========================================================================
 
 function makePedPlayAnimation(ped, animationSlot, positionOffset) {
-	let animationData = getAnimationData(animationSlot);
-	let freezePlayer = false;
-	//if(animationData[9] != VRR_ANIMMOVE_NONE) {
-		switch(animationData[9]) {
-			case VRR_ANIMMOVE_FORWARD:
-				setElementCollisionsEnabled(ped, false);
-				setElementPosition(ped, getPosInFrontOfPos(getElementPosition(ped), fixAngle(getElementHeading(ped)), positionOffset));
-				freezePlayer = true;
-				break;
-
-			case VRR_ANIMMOVE_BACK:
-				setElementCollisionsEnabled(ped, false);
-				setElementPosition(ped, getPosBehindPos(getElementPosition(ped), fixAngle(getElementHeading(ped)), positionOffset));
-				freezePlayer = true;
-				break;
-
-			case VRR_ANIMMOVE_LEFT:
-				setElementCollisionsEnabled(ped, false);
-				setElementPosition(ped, getPosToLeftOfPos(getElementPosition(ped), fixAngle(getElementHeading(ped)), positionOffset));
-				freezePlayer = true;
-				break;
-
-			case VRR_ANIMMOVE_RIGHT:
-				setElementCollisionsEnabled(ped, false);
-				setElementPosition(ped, getPosToRightOfPos(getElementPosition(ped), fixAngle(getElementHeading(ped)), positionOffset));
-				freezePlayer = true;
-				break;
-		}
-	//}
-
-	setEntityData(ped, "vrr.animation", animationData[1], animationData[2]);
-	sendNetworkEventToPlayer("vrr.pedAnim", null, ped.id, animationData[1], animationData[2], animationData[3], animationData[4], animationData[5], positionOffset, freezePlayer);
+	setEntityData(ped, "vrr.anim", animationSlot, true);
+	sendNetworkEventToPlayer("vrr.pedAnim", null, getPedForNetworkEvent(ped), animationSlot, positionOffset);
 }
 
 // ===========================================================================
 
 function makePedStopAnimation(ped) {
-	removeEntityData(ped, "vrr.animation");
-	sendNetworkEventToPlayer("vrr.pedStopAnim", null, ped.id);
+	removeEntityData(ped, "vrr.anim");
+	sendNetworkEventToPlayer("vrr.pedStopAnim", null, getPedForNetworkEvent(ped));
 }
 
 // ===========================================================================
 
-function forcePedAnimation(ped, animationSlot) {
-	let animationData = getAnimationData(animationSlot);
-
-	removeEntityData(ped, "vrr.animation", [animationData[1], animationData[2]], true);
-	sendNetworkEventToPlayer("vrr.forcePedAnim", null, ped.id, animationData[1], animationData[2], animationData[3], animationData[4]);
+function forcePedAnimation(ped, animationSlot, positionOffset = 0) {
+	addEntityData(ped, "vrr.anim", animationSlot, true);
+	sendNetworkEventToPlayer("vrr.forcePedAnim", null, getPedForNetworkEvent(ped), animationSlot, positionOffset);
 }
 
 // ===========================================================================

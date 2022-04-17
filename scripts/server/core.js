5565fa527f7988bc6328c419a0e9d75a735b2458
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-04-16 21:47:49 -0500

    Move locales and accents to server data, not config

--- scripts/server/core.js
+++ scripts/server/core.js
@@ -33,6 +33,8 @@ let serverData = {
 	cachedTranslationFrom: [],
 	triggers: [],
 	npcs: [],
+	locales: [],
+	accents: [],
 };
 
 // ===========================================================================

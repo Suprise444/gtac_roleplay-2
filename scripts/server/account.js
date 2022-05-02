05e58874dcdf31e1a87fe468963fea18804cba22
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-05-02 15:26:39 -0500

    Fix GUI toggle cmd

--- scripts/server/account.js
+++ scripts/server/account.js
@@ -92,16 +92,17 @@ function toggleAutoSelectLastCharacterCommand(command, params, client) {
 // ===========================================================================
 
 function toggleAccountGUICommand(command, params, client) {
+	// Remember, the flag is BACKWARD. Enabled = NO GUI!
 	let flagValue = getAccountSettingsFlagValue("NoGUI");
 
 	if(doesPlayerHaveGUIEnabled(client)) {
-		getPlayerData(client).accountData.settings = removeBitFlag(getPlayerData(client).accountData.settings, flagValue);
-		messagePlayerNormal(client, getLocaleString(client, "GUIAccountSettingToggle", `{softGreen}${toUpperCase(getLocaleString(client, "On"))}{MAINCOLOUR}`));
-		logToConsole(LOG_DEBUG, `[VRR.Account] ${getPlayerDisplayForConsole(client)} has toggled GUI for their account ON.`);
-	} else {
 		getPlayerData(client).accountData.settings = addBitFlag(getPlayerData(client).accountData.settings, flagValue);
 		messagePlayerNormal(client, getLocaleString(client, "GUIAccountSettingToggle", `{softRed}${toUpperCase(getLocaleString(client, "Off"))}{MAINCOLOUR}`));
 		logToConsole(LOG_DEBUG, `[VRR.Account] ${getPlayerDisplayForConsole(client)} has toggled GUI for their account OFF.`);
+	} else {
+		getPlayerData(client).accountData.settings = removeBitFlag(getPlayerData(client).accountData.settings, flagValue);
+		messagePlayerNormal(client, getLocaleString(client, "GUIAccountSettingToggle", `{softGreen}${toUpperCase(getLocaleString(client, "On"))}{MAINCOLOUR}`));
+		logToConsole(LOG_DEBUG, `[VRR.Account] ${getPlayerDisplayForConsole(client)} has toggled GUI for their account ON.`);
 	}
 
 	if(!isPlayerLoggedIn(client)) {
